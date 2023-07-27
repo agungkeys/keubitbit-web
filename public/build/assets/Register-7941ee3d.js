6f2d699d57ce4af20e0e9994e1075f2300418bf6
Agung Kurniawan <agung41149@gmail.com>
2023-07-27 19:23:13 +0700

    add build

--- public/build/assets/Register-7941ee3d.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-05ce5be0.js";const t=()=>e.jsx("h1",{children:"This is Register component"});export{t as default};
---
+++ public/build/public/build/assets/Login-6b964256.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-a9444ed3.js";const s=()=>o.jsx("h1",{children:"This is Login component"});export{s as default};
