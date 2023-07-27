312d5647ab7ad56f29181906280cee3e9e75d13d
Agung Kurniawan <agung41149@gmail.com>
2023-07-27 18:44:02 +0700

    add cdn image

---
+++ build/assets/Register-7941ee3d.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-05ce5be0.js";const t=()=>e.jsx("h1",{children:"This is Register component"});export{t as default};
---
+++ build/assets/Test-b754bddd.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-05ce5be0.js";const e=()=>t.jsx("h1",{children:"This is test component"});export{e as default};
