1a27baf8dc9f2e20dd536f9bee64a7f7ab2e7418
Agung Kurniawan <agung41149@gmail.com>
2023-07-27 19:53:20 +0700

    add build #2

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    build: {
-        outDir: "public/build"
-    },
     plugins: [
         laravel({
             input: [
