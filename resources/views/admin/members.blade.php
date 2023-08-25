1342c7f7194119916fd33b86c7d50d6cba3565e1
Agung Kurniawan <agung41149@gmail.com>
2023-08-25 15:39:02 +0700

    - fix search on member input

--- resources/views/admin/members.blade.php
+++ resources/views/admin/members.blade.php
@@ -8,7 +8,7 @@
       <div class="flex justify-between items-center pb-6">
         <form action="{{ route('admin.members', request()->query()) }}">
           <div class="flex my-2">
-            <input type="text" name="q" placeholder="Search" class="py-2 px-2 text-md border border-gray-200 rounded-l focus:outline-none" value="" />
+            <input type="text" name="q" placeholder="Search" class="py-2 px-2 text-md border border-gray-200 rounded-l focus:outline-none" value="{{ $searchParam }}" />
             <button type="submit" class="btn btn-primary rounded-l-none">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
