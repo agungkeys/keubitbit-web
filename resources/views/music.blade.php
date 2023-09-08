ec9025a6f59759e0aaef5e0b5b6de2b0ab2e59b1
Agung Kurniawan <agung41149@gmail.com>
2023-09-08 22:23:50 +0700

    - fix meta title seo

--- resources/views/music.blade.php
+++ resources/views/music.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app') 
-@section('title', 'About - Keubitbit Aceh Ethnic Music - Official Website')
+@section('title', 'Music - Keubitbit Aceh Ethnic Music - Official Website')
 @section('content')
   <section class="bg-bottom bg-cover bg-[url('https://res.cloudinary.com/domqavi1p/image/upload/c_crop,h_340,w_1200/v1690537335/IMG20230623185903_y1nptl.webp')]">
     <div class="p-5 md:p-10 py-7 md:py-[7em] justify-center items-center" style="background: rgba(59, 38, 6, 0.5)">
