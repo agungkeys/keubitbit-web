b5574605508b2c6f7a8a773881a1d9749d6c88f7
fsvhino <ferrysafrizal20@gmail.com>
2023-11-07 00:03:23 +0800

    -fix save button gallery -improve component form action

--- resources/views/components/form-action.blade.php
+++ resources/views/components/form-action.blade.php
@@ -1,6 +1,6 @@
 @php
   $route = $route ?? '/dashboard';
-  $type = $type ?? 'update';
+  $type = $type;
   $closeUrl = $closeUrl ?? $route;
 @endphp
 
@@ -8,6 +8,8 @@
   <a href="{{ $closeUrl }}" class="btn btn-light">Close</a>
   @if ($type == 'save')
     <button id="submitAdd" type="submit" class="btn btn-primary">Save<span id="loadingAdd" class="loading loading-spinner loading-sm hidden"></span></button>
+  @elseif ($type == 'photo')
+    <button id="submitAddPhoto" type="submit" class="btn btn-primary">Save<span id="loadingAddPhoto" class="loading loading-spinner loading-sm hidden"></span></button>
   @else
     <button id="submitEdit" type="submit" class="btn btn-primary">Update<span id="loadingEdit" class="loading loading-spinner loading-sm hidden"></span></button>
   @endif
